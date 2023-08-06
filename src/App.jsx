7a9d549d0654c43639e22a96c8f1dac51fd6b566
JuanRaV <juanravirgen@gmail.com>
2023-08-06 12:00:14 -0600

    Starting first components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
-
+import Header from './components/Header'
+import Formulario from './components/Formulario'
+import ListadoPacientes from './components/ListadoPacientes'
 
 function App() {
+
   return (
-    <div className='App'>
-      <h1>Hola mundo</h1>
-    </div>
+    <>
+      <Header/>
+      <Formulario/>
+      <ListadoPacientes/>
+    </>
+
   )
 }
 
