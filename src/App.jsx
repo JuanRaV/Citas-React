625a19198f9c67290310756f664b271f8d7c9657
JuanRaV <juanravirgen@gmail.com>
2023-08-06 19:48:50 -0600

    Making the form

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import Header from './components/Header'
 import Formulario from './components/Formulario'
 import ListadoPacientes from './components/ListadoPacientes'
@@ -6,11 +5,14 @@ import ListadoPacientes from './components/ListadoPacientes'
 function App() {
 
   return (
-    <>
+    <div className='container mx-auto mt-20'>
       <Header/>
-      <Formulario/>
-      <ListadoPacientes/>
-    </>
+      <div className='mt-12 md:flex'>
+        <Formulario/>
+        <ListadoPacientes/>
+      </div>
+
+    </div>
 
   )
 }
